b9359d9b40662c8ada287ad6931b563d30fd67fc
Josiah Smith <jcsmithf22@gmail.com>
2024-09-25 17:54:36 -0500

    change to google hosted inter

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -14,7 +14,11 @@
     <link rel="shortcut icon" href="{{ url(asset('favicon.ico')) }}">
 
     <!-- Fonts -->
-    <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
 
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     @livewireStyles
